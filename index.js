d4b1c5e0f028e94d3033486bd93d08eb06eeabcd
Vasudev Kesharwani <kesharwanivasudev@gmail.com>
2022-01-21 09:26:31 -0800

    done

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 
-const Http = new XMLHttpRequest();
-const url='https://api.thingspeak.com/update?api_key=5KWJ1LQXGBSKU5O6&field1=1';
-var clickAudio = new Audio("music/switch-2.mp3");
 var btn_status = 0;
 
-$("button").on("click",function(){
-    clickAudio.play();
-if(btn_status == 0){
- this.innerHTML ="IOT- 'ON' ";
- $("button").addClass("pressed");
- url = url + "1";
- Http.open("GET", url);
- Http.send();
- Http.onreadystatechange = (e) => {
-   alert(Http.responseText);
- }
- btn_status = 1;
-}
-else if( btn_status=== 1){
-    this.innerHTML ="IOT-'OFF'";
-    $("button").removeClass("pressed");
- url = url + "1";
- Http.open("GET", url);
- Http.send();
- Http.onreadystatechange = (e) => {
-   alert(Http.responseText);
-    btn_status =0;
-}
-}
+$("button").on("click", function () {
+    const Http = new XMLHttpRequest();
+    const url = 'https://api.thingspeak.com/update?api_key=5KWJ1LQXGBSKU5O6&field1=1';
+playSound();
+    if (btn_status == 0) {
+        this.innerHTML = "IOT- 'ON' ";
+        $("button").addClass("pressed");
+        btn_status = 1;
+        url = url + "1";
+        Http.open("GET", url);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+            alert(Http.responseText);
+        }
+    }
+    else if (btn_status == 1) {
+        this.innerHTML = "IOT-'OFF'";
+        $("button").removeClass("pressed");
+        btn_status = 0;
+        url = url + "1";
+        Http.open("GET", url);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+            alert(Http.responseText);
+        }
+    }
 });
+function playSound(){
+var clickAudio = new Audio("music/switch-2.mp3");
+clickAudio.play();
+}
