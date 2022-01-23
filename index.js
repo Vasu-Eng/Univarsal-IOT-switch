8affb0467994704084b8fc9737743e33bcac4369
Vasudev Kesharwani <kesharwanivasudev@gmail.com>
2022-01-23 04:44:03 -0800

    successfully solved the bug

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const Http = new XMLHttpRequest();
-var btn_status = 0;
+var btn_status = -1;
 var res;
-$("button").on("click", function () {
+var resLog;
+
+function myFunction() {
+    Http.open("GET", "https://api.thingspeak.com/channels/1637458/fields/1.json?results=1");
+    Http.send();
+    Http.onreadystatechange = function () {
+        if (this.status == 200) {
+            resLog = JSON.parse(this.responseText);
+            btn_status = Number(resLog.feeds[0].field1);
+            console.log("the current status in clond : " + btn_status);
+            switch (btn_status) {
+                case 1: {
+                    $("button").addClass("pressed");
+                    $("button").html("STATUS-  ON");
+                }
+                    break;
+                case 0: {
+                    $("button").html("STATUS- OFF");
+                    $("button").removeClass("pressed");
+                }
+                    break;
+
+            }
+        }
+    }
+}
+
+$("button").on("click", function () { clickHandler() });
+
+function playSound() {
+    var clickAudio = new Audio("music/switch-2.mp3");
+    clickAudio.play();
+}
+
+function clickHandler() {
     if (btn_status == 0) {
         Http.open("GET", "https://api.thingspeak.com/update.json?api_key=5KWJ1LQXGBSKU5O6&field1=1");
         Http.send();
@@ -34,8 +68,4 @@ $("button").on("click", function () {
             }
         }
     }
-});
-function playSound() {
-    var clickAudio = new Audio("music/switch-2.mp3");
-    clickAudio.play();
 }
