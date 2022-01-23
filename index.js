6b4aabf73e1aafc031c1988a635cb6287d281b70
Vasudev Kesharwani <kesharwanivasudev@gmail.com>
2022-01-23 01:04:40 -0800

    successfully solved the bug

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 const Http = new XMLHttpRequest();
 var btn_status = 0;
-var res_1;
-var res_0;
+var res = -1;
 $("button").on("click", function () {
     if (btn_status == 0) {
-        btn_status = 1;
-        Http.open("GET", "https://api.thingspeak.com/update?api_key=5KWJ1LQXGBSKU5O6&field1=1");
+        Http.open("GET", "https://api.thingspeak.com/update.json?api_key=5KWJ1LQXGBSKU5O6&field1=1");
         Http.send();
-        Http.onreadystatechange = (e) => {
-         res_1 = Http.responseText;
-         console.log( res_1);
-        }
-        if( res_1 !== "0"){
-            this.innerHTML = "STATUS- 'ON' ";
-            $("button").addClass("pressed");
-            playSound();
+        Http.onreadystatechange = function () {
+            if (this.status == 200) {
+                res = JSON.parse(this.responseText);
+                console.log("switch ON  : ", res.entry_id);
+                if (res.entry_id) {
+                    $("button").addClass("pressed");
+                    $("button").html("STATUS-  ON");
+                    playSound();
+                    btn_status = 1;
+                }
+            }
         }
     }
     else if (btn_status == 1) {
-        $("button").removeClass("pressed");
-        btn_status = 0;
-        Http.open("GET", "https://api.thingspeak.com/update?api_key=5KWJ1LQXGBSKU5O6&field1=0");
+        Http.open("GET", "https://api.thingspeak.com/update.json?api_key=5KWJ1LQXGBSKU5O6&field1=0");
         Http.send();
-        Http.onreadystatechange = (e) => {
-            res_0 = Http.responseText;
-            console.log( res_0);
-        }
-        if( res_0 !== "0"){
-            this.innerHTML = "STATUS-'OFF'";
-            $("button").removeClass("pressed");
-            playSound();
+        Http.onreadystatechange = function () {
+            if (this.status == 200) {
+                res = JSON.parse(this.responseText);
+                console.log("switch OFF : ", res.enrty_id);
+                if (res.entry_id) {
+                    $("button").html("STATUS- OFF");
+                    $("button").removeClass("pressed");
+                    playSound();
+                    btn_status = 0;
+                }
+            }
         }
     }
 });
-function playSound(){
-var clickAudio = new Audio("music/switch-2.mp3");
-clickAudio.play();
+function playSound() {
+    var clickAudio = new Audio("music/switch-2.mp3");
+    clickAudio.play();
 }
